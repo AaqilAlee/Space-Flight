3b3b4378d2589ba62fa9a2a0b23e2f6069ff5d11
Mamun13 <mamuncse82@gmail.com>
2024-01-11 14:54:13 +0600

    update filter

--- src/Components/flight/SpaceFlight.jsx
+++ src/Components/flight/SpaceFlight.jsx
@@ -1,14 +1,13 @@
-import React,{ useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import FlightCard from "./FlightCard";
+import "./FlightCard.css";
 
 const SpaceFlight = () => {
-  const [search, setSearch] = useState('');
-  const [status, setStatus] = useState('');
+  const [search, setSearch] = useState("");
+  const [status, setStatus] = useState("");
   const [data, setData] = useState([]);
-  const [filter, setFilter] = useState({ status: status });
 
   const getData = async () => {
     const response = await fetch("https://api.spacexdata.com/v3/launches");
@@ -19,16 +18,32 @@ const SpaceFlight = () => {
   useEffect(() => {
     getData();
   }, []);
+  const [filterSuccess, setFilterSuccess] = useState(data);
+  const formatDate = (d) => {
+    const date = new Date(d);
+    return date.toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+    });
+  };
+
   useEffect(() => {
-    setFilter({...filter, status: status})
-  })
- 
-  const filteredLaunches = data.filter((launch) => {
-    if (search !== "f") {
-      const searchLowerCase = search.toLowerCase();
-      return launch.mission_name.toLowerCase().includes(searchLowerCase);
+    const filteredSearch = data.filter((item) => {
+      return item.mission_name.toLowerCase().includes(search.toLowerCase());
+    });
+
+    if (status === "") {
+      setFilterSuccess(filteredSearch);
+    } else {
+      const updatedList = filteredSearch.filter(
+        (curStatus) => curStatus.launch_success === (status === "success")
+      );
+      setFilterSuccess(updatedList);
     }
-  });
+  }, [status, data, search]);
+
+  
 
   return (
     <>
@@ -52,11 +67,15 @@ const SpaceFlight = () => {
                       className="form-control search_form"
                       placeholder="search in here"
                       value={search}
-                      onChange={(e)=>setSearch(e.target.value)}
+                      onChange={(e) => setSearch(e.target.value)}
                     />
-                    <Button variant="primary" className="search_form2" onClick={() => setSearch(search)}>
+                    <Button
+                      variant="primary"
+                      className="search_form2"
+                      onClick={() => setSearch(search)}
+                    >
                       Search
-                    </Button>{" "}
+                    </Button>
                   </div>
                 </Col>
                 <Col lg={6} md={6}>
@@ -66,14 +85,14 @@ const SpaceFlight = () => {
                         aria-label="Default select example"
                         className=" mb-3 rounded-1"
                         value={status}
-                        onChange={e => setStatus(e.target.value)}
+                        onChange={(e) => setStatus(e.target.value)}
                       >
-                        <option>Select status</option>
-                        <option value="all">All</option>
+                        <option value={""}>Select status</option>
                         <option value="success">Success</option>
-                        <option value="failed">failure</option>
+                        <option value="failure">failure</option>
                       </Form.Select>
                     </Col>
+
                     <Col lg={6} md={6}>
                       <Form.Select
                         aria-label="Default select example"
@@ -90,7 +109,55 @@ const SpaceFlight = () => {
               </Row>
             </Form>
           </div>
-          <FlightCard search={search} status={status} data={filteredLaunches}/>
+          {/* <FlightCard search={search} status={status} data={filteredLaunches}/> */}
+        </Container>
+      </section>
+      <section className="mt-5 px-1">
+        <Container>
+          <Row>
+            {filterSuccess.map((curElem, index) => {
+              return (
+                <>
+                  <Col lg={4} md={6} className="mb-3" key={index}>
+                    <div className="border rounded-1 ">
+                      <div>
+                        <img
+                          src={curElem?.links?.mission_patch_small || ""}
+                          alt=""
+                          className="img-fluid card_img my-4"
+                        />
+                        <p className="text-center mb-0  text-secondary">
+                          Launch date:{" "}
+                          {formatDate(curElem?.launch_date_local) || ""}
+                        </p>
+                        <h4 className="text-center text-capitalize">
+                          {curElem?.mission_name || ""}
+                        </h4>
+                        <p className="text-center mb-0 text-secondary">
+                          {curElem?.rocket?.rocket_id || ""}
+                        </p>
+
+                        <h4 className="text-center text-capitalize text-secondary mt-4">
+                          launch status:
+                        </h4>
+                        <p className="text-center mt-4">
+                          {curElem?.launch_success ? (
+                            <span className="text-center text-light fw-bold px-3 py-1 rounded text-capitalize bg-success">
+                              Success
+                            </span>
+                          ) : (
+                            <span className="text-center text-light fw-bold px-3 py-1 rounded text-capitalize bg-danger">
+                              Failed
+                            </span>
+                          )}
+                        </p>
+                      </div>
+                    </div>
+                  </Col>
+                </>
+              );
+            })}
+          </Row>
         </Container>
       </section>
     </>
